32d3eaa24b79fd2442f1b6ef749ebbe1e2dfee4f
koki <shintech9898@gmail.com>
2025-06-13 13:45:44 +0900

    paginate_by = 20に修正

--- item/views.py
+++ item/views.py
@@ -8,7 +8,7 @@ class ItemListView(ListView):
     model = Item
     template_name = 'item/item_list.html'
     context_object_name = 'items'  # テンプレートで使用するコンテキスト変数名
-    paginate_by = 8  # 1ページあたりのアイテム数
+    paginate_by = 20  # 1ページあたりのアイテム数
 
     def get_queryset(self):
         queryset = super().get_queryset() # デフォルトのクエリセット（全商品）を取得

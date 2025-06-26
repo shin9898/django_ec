73bc8b9c90e6b6978d5354e8098562c4070e1ab2
koki <shintech9898@gmail.com>
2025-06-26 23:28:48 +0900

    cart_listのurl修正

--- cart/urls.py
+++ cart/urls.py
@@ -4,7 +4,7 @@ from .views import CartListView, AddToCartView, RemoveFromCartView, UpdateCartVi
 app_name = 'cart'
 # URL configuration for the item app
 urlpatterns = [
-    path('list/', CartListView.as_view(), name='cart_list'),
+    path('', CartListView.as_view(), name='cart_list'),
     path('add_to_cart/<int:item_id>/', AddToCartView.as_view(), name='add_to_cart'),
     path('remove/<int:item_id>/', RemoveFromCartView.as_view(), name='remove_from_cart'),
     path('update/<int:item_id>/', UpdateCartView.as_view(), name='update_cart'),

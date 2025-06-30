8a2f444db8e0a7df0751fd0f3bbe15279b24d739
koki <shintech9898@gmail.com>
2025-06-30 20:25:18 +0900

    Brevo SMTPに変更

--- config/settings.py
+++ config/settings.py
@@ -161,22 +161,23 @@ BASICAUTH_USERS= {
     env('BASICAUTH_USERNAME'): env('BASICAUTH_PASSWORD')
 }
 
-# メール設定（Gmail SMTP）
+# メール設定
 if DEBUG:
     # 開発環境：コンソール出力
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'test@localhost.com'
 else:
-    # 本番環境：Gmail SMTP
+    # 本番環境：Brevo SMTP
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST = 'smtp-relay.brevo.com'
     EMAIL_PORT = 587
     EMAIL_USE_TLS = True
-    EMAIL_HOST_USER = os.environ.get('GMAIL_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('GMAIL_APP_PASSWORD')
-    DEFAULT_FROM_EMAIL = os.environ.get('GMAIL_USER')
+    EMAIL_HOST_USER = os.environ.get('BREVO_EMAIL')
+    EMAIL_HOST_PASSWORD = os.environ.get('BREVO_SMTP_KEY')
+    DEFAULT_FROM_EMAIL = os.environ.get('BREVO_EMAIL')
 
 EMAIL_SUBJECT_PREFIX = '[Happiness Shop]'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

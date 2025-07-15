0a5f9f1d748d836fa66f310c38ee87d640bcfae3
koki <shintech9898@gmail.com>
2025-07-15 12:17:56 +0900

    forms.py: 不要なMetaクラスのwidgetsとlabelsオプションを削除

--- item/forms.py
+++ item/forms.py
@@ -5,19 +5,6 @@ class ItemForm(forms.ModelForm):
     class Meta:
         model = Item
         fields = ['name', 'description', 'price', 'image', 'sku', 'stock', 'is_published']
-        widgets = {
-            'description': forms.Textarea(attrs={'rows': 4, 'cols': 40}),
-            'price': forms.NumberInput(attrs={'min': '1'}),
-        }
-        labels = {
-            'name': '商品名',
-            'description': '商品説明',
-            'price': '価格',
-            'image': '商品画像',
-            'sku': '商品コード/SKU',
-            'stock': '在庫数',
-            'is_published': '公開状態',
-        }
 
     def clean_price(self):
         price = self.cleaned_data.get('price')
